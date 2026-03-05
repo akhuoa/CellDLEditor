737c7f8c82301043bd4de3fd051c024d07df8d47
David Brooks <dbrnz@users.noreply.github.com>
2026-03-05 20:15:30 +1300

    We no longer publish the Vue component from this repo but instead deploy the app to GH Pages.

--- src/renderer/deploy.sh
+++ src/renderer/deploy.sh
@@ -4,4 +4,4 @@ git push origin
 git push origin v$1
 gh release create v$1 --verify-tag --title "Release $1" --notes ""
 
-gh workflow run publish.yaml --ref v$1
+gh workflow run deploy.yaml --ref v$1
